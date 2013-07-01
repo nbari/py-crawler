06b1621960d1dbe4f130e4a6be19c3fb75740c3b
nbari <nbari@dalmp.com>
2013-07-01 14:17:01 +0100

    modified:   crawler.py

--- crawler.py
+++ crawler.py
@@ -5,16 +5,15 @@
 
 import os
 import gevent
+import urllib2
+import time
 from gevent import monkey
+from bs4 import BeautifulSoup
 monkey.patch_all()
 
-""" to parse the page """
-from bs4 import BeautifulSoup # To get everything
+urls = ['http://www.sabado.pt', 'http://www.sapo.pt', 'http://www.visao.pt',
+        'http://www.sign.io', 'http://www.ddns.pt', 'http://news.google.pt', 'http://www.dn.pt']
 
-urls = ['http://www.sabado.pt','http://www.sapo.pt','http://www.visao.pt','http://www.sign.io','http://www.ddns.pt','http://news.google.pt','http://www.dn.pt']
-
-import urllib2
-from datetime import datetime
 
 def makeSoup(url):
     print ('Starting %s' % url)
@@ -26,19 +25,18 @@ def makeSoup(url):
     # imgs = [x['src'] for x in soup.findAll('img')]
 
     """ prepare the dir to store all the data """
-    dir_name = 'pages/'+''.join(url.split('//')[1:])
+    dir_name = 'pages/' + ''.join(url.split('//')[1:])
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
     """ write extracted html """
-    f = open(dir_name + '/html.txt' ,'w')
-    data =  html.prettify().encode('UTF-8')
+    f = open(dir_name + '/html.txt', 'w')
+    data = html.prettify().encode('UTF-8')
     f.write(data)
     f.close()
     print ('%s: %s bytes' % (url, len(page)))
 
-startTime = datetime.now()
+startTime = time.time()
 jobs = [gevent.spawn(makeSoup, url) for url in urls]
 gevent.joinall(jobs)
-totalTime = datetime.now() - startTime
-print "Total time: %s" % totalTime
+print "Total time: %s" % (time.time() - startTime)
